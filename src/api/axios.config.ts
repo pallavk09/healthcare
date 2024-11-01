30c4648775ad056d4c064d6c36c50251565634c3
kpallav06 <kpallav06@gmail.com>
2024-11-01 17:03:18 +0530

    API url changed to deployed one

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_BASE_URL = "http://127.0.0.1:3002/api/v1";
 const API_BASE_URL =
-  "https://backends-o9sqbdbgy-pallav-kumars-projects-a237a5e7.vercel.app/api/v1";
+  "https://backends-heybnytn5-pallav-kumars-projects-a237a5e7.vercel.app/api/v1";
 
 const axios_instance = axios.create({
   baseURL: API_BASE_URL,
