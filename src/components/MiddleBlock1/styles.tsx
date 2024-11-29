b53d3855cd47af156cac0cf656d906df68d08778
kpallav06 <kpallav06@gmail.com>
2024-11-29 14:14:06 +0530

    padding for MiddleBlock1 increased

--- src/components/MiddleBlock1/styles.tsx
+++ src/components/MiddleBlock1/styles.tsx
@@ -8,7 +8,7 @@ export const MiddleBlockSection = styled("section")`
   justify-content: left;
 
   @media screen and (max-width: 1024px) {
-    padding: 2rem 0 3rem;
+    padding: 4rem 0 3rem;
   }
 `;
 
